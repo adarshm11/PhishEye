817ccf2c07b40e92e6d3b9ea6bb765b2a6a319c0
adarshm11 <amlearning2023@gmail.com>
2025-03-23 22:23:17 -0700

    Built framework for frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,29 @@ import './App.css';
 function App() {
 
   return (
-    <h1>Hello</h1>
+    <div className="app-container">
+      <div className="nav-bar-container">
+        <nav>
+
+        </nav>
+      </div>
+      <div className="body-container">
+        <div className="title-container">
+          <h1>PhishShield</h1>
+          <p>Description</p>
+        </div>
+        <div className="main-content-container">
+          <div className="input-container">
+            <form>
+              <input/>
+            </form>
+          </div>
+          <div className="result-container">
+            { /* Return from API call */ }
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
